a27faf1bc749c072020cfea14bab8ebb9025604b
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-23 14:31:26 -0500

    add structs and padding

---
+++ Files/StructsAndPadding/binary_files.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#pragma pack(push, 1)
+
+struct Person {
+    char name[50];
+    int age;
+    double weight;
+};
+
+#pragma pack(pop)
+
+int main() {
+
+    cout << sizeof(Person) << endl;
+
+    return 0;
+}
