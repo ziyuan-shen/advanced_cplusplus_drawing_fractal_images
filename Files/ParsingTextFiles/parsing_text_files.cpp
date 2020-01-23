42b5f84dd706fb52891c3e8801992ea0690488a7
Ziyuan Shen <ziyuan.shen@duke.edu>
2020-01-23 13:30:46 -0500

    add parsing text files

---
+++ Files/ParsingTextFiles/parsing_text_files.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    string filename = "stats.txt";
+    fstream input;
+
+    input.open(filename);
+
+    if(!input.is_open()) {
+        return 1;
+    }
+
+    while(input) {
+        string line;
+
+        getline(input, line, ':');
+
+        int population;
+        input >> population;
+
+        input.get();
+
+        if(!input) {
+            break;
+        }
+
+        cout << line << "--" << population << endl;
+    }
+
+    input.close();
+
+    return 0;
+}
